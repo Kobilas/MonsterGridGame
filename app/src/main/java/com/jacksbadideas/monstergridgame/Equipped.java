eb456f55ecba1507de79801201703d1324e56d02
mattkobi789 <mattkobi789@gmail.com>
2017-10-05 02:32:22 -0400

    Finished adding ArrayLists to Equipped class

--- app/src/main/java/com/jacksbadideas/monstergridgame/Equipped.java
+++ app/src/main/java/com/jacksbadideas/monstergridgame/Equipped.java
@@ -8,18 +8,39 @@ import java.util.ArrayList;
 
 public class Equipped {
     private ArrayList<Equipment> equipped;
+    private ArrayList<Weapon> equippedWeapon;
+
     public Equipped(){
         equipped = new ArrayList<Equipment>();
+        equippedWeapon = new ArrayList<Weapon>();
     }
     public Equipped(Equipment initEquipment){
         equipped = new ArrayList<Equipment>();
         equipped.add(initEquipment);
+        equippedWeapon = new ArrayList<Weapon>();
+    }
+    public Equipped(Weapon initWeapon){
+        equippedWeapon = new ArrayList<Weapon>();
+        equippedWeapon.add(initWeapon);
+        equipped = new ArrayList<Equipment>();
+    }
+    public Equipped(Equipment initEquipment, Weapon initWeapon){
+        equipped = new ArrayList<Equipment>();
+        equipped.add(initEquipment);
+        equippedWeapon = new ArrayList<Weapon>();
+        equippedWeapon.add(initWeapon);
     }
     public void equip(Equipment newEquipment){
         equipped.add(newEquipment);
     }
     public boolean unequip(Equipment toRemove){
         return equipped.remove(toRemove);
+    }
+    public void equip(Weapon newWeapon){
+        equippedWeapon.add(newWeapon);
+    }
+    public boolean unequip(Weapon toRemove){
+        return equippedWeapon.remove(toRemove);
     }
     public int getTotalDefence(){
         int totalDefence = 0;
@@ -33,5 +54,10 @@ public class Equipped {
             totalMagicDefence += equipped.get(i).getMagicDefence();
         return totalMagicDefence;
     }
-
+    public int getTotalAttack() {
+        int totalAttack = 0;
+        for (int i = 0; i < equippedWeapon.size(); i++)
+            totalAttack += equippedWeapon.get(i).getAttack();
+        return totalAttack;
+    }
 }
